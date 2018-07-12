6502aaa726f46d28c7265da5f4469d142dd3d519
Jared Blitzstein <jcblitz@users.noreply.github.com>
2018-07-11 22:11:49 -0400

    switched over to war deploy for tomcat

--- src/main/java/nl/revolution/example/SampleApplication.java
+++ src/main/java/nl/revolution/example/SampleApplication.java
@@ -2,9 +2,10 @@ package nl.revolution.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
